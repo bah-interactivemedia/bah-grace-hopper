002a7a0876771ae9d9d0580ec1869fd9cbfdaa75
Nat Burgwyn <burgwyn@gmail.com>
2016-12-09 14:29:50 -0500

    Update

--- assets/js/application.js
+++ assets/js/application.js
@@ -11,7 +11,7 @@
      
      $.getJSON('/data/repos.json', function(data){
          $(data).each(function(i, e){
-             $("#ul-search-results").append('<li class="results-source"><small>Source</small> <a href="#">' + e.name + '</a></li><li class="results-languages"><small>Language</small><a href="#">' + e.lang + '</a></li>');
+             $(".usa-media-block-inner").append('<h3 class="results-title">' + e.name + '</h3><p class="results-description">' + e.description + '</p>');
          });
      });
  }
