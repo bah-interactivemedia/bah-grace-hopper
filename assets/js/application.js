8d5303cc4b7b236eda0b2c315485b55bbd39a571
Nat Burgwyn <burgwyn@gmail.com>
2016-12-09 14:16:12 -0500

    Search

--- assets/js/application.js
+++ assets/js/application.js
@@ -7,10 +7,11 @@
  -{% include_relative vendor/uswds.min.js %}
 
  // Add your JS sripts here
- function getSearch() {
-     //evt.preventDefault();
-     var q = document.getElementById('search-field-big');
-
-     console.log(q.value);
-     return false;
+ function getSearchResults() {
+     
+     $.getJSON('/data/repos.json', function(data){
+         $(data).each(function(i, e){
+             $("#ul-search-results").append('<li class="results-source"><small>Source</small> <a href="#">' + e.name + '</a></li><li class="results-languages"><small>Language</small><a href="#">' + e.lang + '</a></li>');
+         });
+     });
  }
